60dc53f6e1fda9b390e63d84596d6d694be4dc12
StefanDue <stefan.duenser@students.fhv.at>
2021-12-11 21:27:05 +0100

    Wrote testbench for the finite state machine door

--- Test_preparation/finite_state_machine/sim/tb_fsm_door_1596.sv
+++ Test_preparation/finite_state_machine/sim/tb_fsm_door_1596.sv
@@ -4,3 +4,114 @@
 // Author:      SteDun
 // Version:     V1.0 2021-12-10
 // ------------------------------------------
+
+module tb_fsm_door_1596();
+
+
+// (1) DUT wiring
+logic       rst_n;
+logic       clk2m;
+logic       key_up;
+logic       key_down;
+logic       sense_up;
+logic       sense_down;
+logic       ml;
+logic       mr;
+logic       light_red;
+logic       light_green;
+
+
+// (2) DUT instance
+fsm_door_1596 dut(.*);
+
+
+
+// (3) DUT stimulation
+logic run_sim = 1'b1;
+
+// define the 2 MHz clock 
+initial begin
+    clk2m = 1'b0;
+    while(run_sim) begin
+        #250ns;             // half of the 500ms clock period
+        clk2m = ~clk2m;
+    end
+end 
+
+// test the dut
+initial begin
+    // set inputs to default value
+    key_up      = 1'b0;
+    key_down    = 1'b0;
+    sense_up    = 1'b0;
+    sense_down  = 1'b1;  
+
+    // start with reset
+    rst_n = 1'b0;
+    #5us;
+    rst_n = 1'b1;
+    #5us;
+
+    // close the door
+    @(negedge clk2m);
+    key_up      = 1'b1;
+    sense_down  = 1'b0;
+    @(negedge clk2m);
+    key_up = 1'b0;
+    #10us;
+
+    // door is open
+    @(negedge clk2m);
+    sense_up    = 1'b1;
+    #5us;
+
+    // door should stay open if key up and key down are pressed at the same time
+    // in this implementation, door open will overrule door close if key_up and key_down is pressed at the same time
+    @(negedge clk2m);
+    key_up      = 1'b1;
+    key_down    = 1'b1;
+    @(negedge clk2m);
+    key_up      = 1'b0;
+    key_down    = 1'b0;
+    #5us;
+
+    // close the door
+    @(negedge clk2m);
+    key_down    = 1'b1;
+    sense_up    = 1'b0;
+    #10us;
+
+    // door is closed
+    @(negedge clk2m);
+    key_down    = 1'b0;
+    sense_down  = 1'b1;
+    #5us;
+
+    // door should open
+    @(negedge clk2m);
+    sense_down  = 1'b0;
+    key_up      = 1'b1; 
+    #10us;
+
+    //open the door again
+    @(negedge clk2m);
+    key_up      = 1'b0;
+    sense_up    = 1'b1;
+    #5us;
+
+    // error occures
+    rst_n = 1'b0;
+    // error overrules everything
+    @(negedge clk2m);
+    key_down = 1'b1;
+    #5us;
+    @(negedge clk2m);
+    key_down = 1'b0;
+
+    #20us;
+    run_sim = 1'b0;
+end
+
+
+
+endmodule
--- Test_preparation/finite_state_machine/src/fsm_door_1596.sv
+++ Test_preparation/finite_state_machine/src/fsm_door_1596.sv
@@ -7,20 +7,20 @@
 
 module fsm_door_1596
 (
-    input   logic       rst_n;
-    input   logic       clk2m;
-    input   logic       key_up;
-    input   logic       key_down;
-    input   logic       sense_up;
-    input   logic       sense_down;
+    input   logic       rst_n,
+    input   logic       clk2m,
+    input   logic       key_up,
+    input   logic       key_down,
+    input   logic       sense_up,
+    input   logic       sense_down,
 
-    output  logic       ml;
-    output  logic       mr;
-    output  logic       light_red;
-    output  logic       light_green;
+    output  logic       ml,
+    output  logic       mr,
+    output  logic       light_red,
+    output  logic       light_green
 );
 
-enum logic [2:0]    {IDLE, UP, OPEN, DOWN, CLOSED} state, next_state;
+enum logic [2:0]    {IDLE, UP, OPEN, DOWN, CLOSED} state, state_next;
 
 
 // --- Sequential logic ---
@@ -29,7 +29,7 @@ always_ff @(negedge rst_n or posedge clk2m) begin
         state <= IDLE;
     end
     else begin
-        state <= next_state;
+        state <= state_next;
     end
 end
 
@@ -42,52 +42,57 @@ always_comb begin
     mr          = 1'b0;
     light_red   = 1'b0;
     light_green = 1'b0;
-    key_up      = 1'b0;
-    key_down    = 1'b0;
-    sense_up    = 1'b0;
-    sense_down  = 1'b0;
 
     case(state)
         IDLE: begin
-            light_red   = 1'b1;
-            if(key_up) begin
+            light_red = 1'b1;
+            if(key_up == 1'b1) begin
                 state_next = UP;
             end
         end
         UP: begin
-            mr          = 1'b1;
-            light_red   = 1'b1
-            if(sense_up) begin
-                state_next = OPEN;
-            end
-        end
-        OPEN: begin
             light_red   = 1'b1;
-            if(key_up) begin
+            mr          = 1'b1;
+            if(sense_up == 1'b1) begin
+                light_red   = 1'b0;
+                mr          = 1'b0;
+                state_next  = OPEN;
+            end
+        end 
+        OPEN: begin
+            light_green = 1'b1;
+            if(key_up == 1'b1) begin
                 state_next = OPEN;
             end
-            else if(key_down) begin
-                state_next = DOWN;
+            else if(key_down == 1'b1) begin
+                state_next      = DOWN;
+                light_green     = 1'b0;
             end
         end
         DOWN: begin
             light_red   = 1'b1;
             ml          = 1'b1;
-            if(sense_down) begin
-                state_next = CLOSED;
+            // open overrules close
+            if((key_up == 1'b1) && (key_down == 1'b1)) begin
+                ml          = 1'b0;
+                state_next  = OPEN;
+            end 
+            else if(sense_down == 1'b1) begin
+                ml          = 1'b0;
+                state_next  = CLOSED;
             end
         end
         CLOSED: begin
-            light_green     = 1'b1;
-            if(key_down) begin
-                state_next = CLOSED;
+            light_red   = 1'b1;
+            if(key_down == 1'b1) begin
+                state_next  = CLOSED;
             end
-            else if(key_up) begin
+            else if(key_up == 1'b1) begin
                 state_next = UP;
             end
         end
         default: begin
-            next_state = IDLE;
+            state_next = IDLE;
         end
     endcase
 end
